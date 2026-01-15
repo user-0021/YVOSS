613e5194936808446e73d0a9fbe56fa525dc3641
user-0021 <nabeyamaoden@gmail.com>
2026-01-15 09:59:59 +0000

    add jsValue

--- lib/HtmlClass/include/html_class.hpp
+++ lib/HtmlClass/include/html_class.hpp
@@ -1,8 +1,9 @@
 #pragma once
 #include <vector>
 #include <string>
+#include <memory>
 
-#define HTML_TAG_FOREACH_BEGIN(...) [&]() -> std::vector<htmlTag>{std::vector<htmlTag> ____________dontuse_tags; for(__VA_ARGS__){____________dontuse_tags = ____________dontuse_tags+[&]() -> std::vector<htmlTag>
+#define HTML_TAG_FOREACH_BEGIN(...) [&]() -> std::vector<HtmlTag>{std::vector<HtmlTag> ____________dontuse_tags; for(__VA_ARGS__){____________dontuse_tags = ____________dontuse_tags+[&]() -> std::vector<HtmlTag>
 #define HTML_TAG_FOREACH_END ();}return ____________dontuse_tags;}()
 
 
@@ -11,80 +12,149 @@ enum TagType{
 	TAG_TYPE_VALUE
 };
 
+
+class HtmlTag;
+class HtmlVal;
+class HtmlPage;
+class JsValue;
+
+
+std::vector<HtmlTag> operator+(const std::vector<HtmlTag>& left  ,std::vector<HtmlTag>&& right) noexcept;
+std::vector<HtmlTag>&& operator+(std::vector<HtmlTag>&& left,std::vector<HtmlTag>&& right) noexcept;
+
+std::vector<HtmlTag> operator+(std::vector<HtmlTag> left,const HtmlTag& right) noexcept;
+std::vector<HtmlTag> operator+(std::vector<HtmlTag> left,HtmlTag&& right) noexcept;
+std::vector<HtmlTag>&& operator+(std::vector<HtmlTag>&& left,const HtmlTag& right) noexcept;
+std::vector<HtmlTag>&& operator+(std::vector<HtmlTag>&& left,HtmlTag&& right) noexcept;
+
+std::vector<HtmlTag> operator+(std::vector<HtmlTag> left,const HtmlVal& right) noexcept;
+std::vector<HtmlTag> operator+(std::vector<HtmlTag> left,HtmlVal&& right) noexcept;
+std::vector<HtmlTag>&& operator+(std::vector<HtmlTag>&& left,const HtmlVal& right) noexcept;
+std::vector<HtmlTag>&& operator+(std::vector<HtmlTag>&& left,HtmlVal&& right) noexcept;
+
+
 /**
  * @brief html構造のtag情報を管理します
  * 
  */
-class htmlTag
+class HtmlTag
 {
 protected:
 	TagType type;
 	std::string tag;
 	std::string option;
 
-	std::vector<htmlTag> children;
+	std::vector<HtmlTag> children;
 public:
-	htmlTag() noexcept:tag(),option(),children(),type(){};
-	htmlTag(const htmlTag& other) noexcept;
-	htmlTag(htmlTag&& other) noexcept;
-	htmlTag(const std::string& tag,const std::string& option) noexcept;
-	htmlTag(std::string&& tag,std::string&& option) noexcept;
-	htmlTag(const std::string& tag,const std::string& option,std::vector<htmlTag> children) noexcept;
-	htmlTag(std::string&& tag,std::string&& option,std::vector<htmlTag> children) noexcept;
-	~htmlTag() noexcept;
+	HtmlTag() noexcept:tag(),option(),children(),type(){};
+	HtmlTag(const HtmlTag& other) noexcept;
+	HtmlTag(HtmlTag&& other) noexcept;
+	HtmlTag(const std::string& tag,const std::string& option) noexcept;
+	HtmlTag(std::string&& tag,std::string&& option) noexcept;
+	HtmlTag(const std::string& tag,const std::string& option,std::vector<HtmlTag> children) noexcept;
+	HtmlTag(std::string&& tag,std::string&& option,std::vector<HtmlTag> children) noexcept;
+	~HtmlTag() noexcept;
 
 	TagType getTagType() const {return this->type;}
 	std::string encord() const;
 	
-	operator std::vector<htmlTag>() const{return std::vector<htmlTag>{*this};}
-	htmlTag& operator=(const htmlTag& other) noexcept;
-	htmlTag& operator=(htmlTag&& other) noexcept;
-	std::vector<htmlTag> operator+(htmlTag& right) noexcept;
-	std::vector<htmlTag> operator+(htmlTag&& right) noexcept;
+	operator std::vector<HtmlTag>() const{return std::vector<HtmlTag>{*this};}
+	HtmlTag& operator=(const HtmlTag& other) noexcept;
+	HtmlTag& operator=(HtmlTag&& other) noexcept;
+	std::vector<HtmlTag> operator+(HtmlTag& right) noexcept;
+	std::vector<HtmlTag> operator+(HtmlTag&& right) noexcept;
 };
 
 /**
  * @brief html構造のタグ内のテキストを管理します
  * 
  */
-class htmlVal : public htmlTag
+class HtmlVal : public HtmlTag
 {
 public:
-	htmlVal(const htmlVal& other) noexcept;
-	htmlVal(htmlVal&& other) noexcept;
-	htmlVal(const std::string& text) noexcept;
-	htmlVal(std::string&& text) noexcept;
-	htmlVal& operator=(htmlVal&& other) noexcept;
-	std::vector<htmlTag> operator+(const htmlVal& right) const noexcept;
-	std::vector<htmlTag> operator+(htmlVal&& right) const noexcept;
-	~htmlVal() noexcept;
+	HtmlVal(const HtmlVal& other) noexcept;
+	HtmlVal(HtmlVal&& other) noexcept;
+	HtmlVal(const std::string& text) noexcept;
+	HtmlVal(std::string&& text) noexcept;
+	HtmlVal& operator=(HtmlVal&& other) noexcept;
+	std::vector<HtmlTag> operator+(const HtmlVal& right) const noexcept;
+	std::vector<HtmlTag> operator+(HtmlVal&& right) const noexcept;
+	~HtmlVal() noexcept;
 };
 
 
-class htmlPage
+class HtmlPage
 {
 private:
-	htmlTag head;
-	htmlTag body;	
+	HtmlTag head;
+	HtmlTag body;	
 public:
-	//htmlPage() noexcept:head(),body(){};
 	std::string encord() const {return head.encord() + body.encord();}
-	void setHead(const htmlTag&  head) noexcept{this->head = head;}
-	void setHead(const htmlTag&& head) noexcept{this->head = std::move(head);}
-	void setBody(const htmlTag&  body) noexcept{this->body = body;}
-	void setBody(const htmlTag&& body) noexcept{this->body = std::move(body);}
+	void setHead(const HtmlTag&  head) noexcept{this->head = head;}
+	void setHead(const HtmlTag&& head) noexcept{this->head = std::move(head);}
+	void setBody(const HtmlTag&  body) noexcept{this->body = body;}
+	void setBody(const HtmlTag&& body) noexcept{this->body = std::move(body);}
 };
 
-std::vector<htmlTag> operator+(const std::vector<htmlTag>& left  ,std::vector<htmlTag>&& right) noexcept;
-std::vector<htmlTag>&& operator+(std::vector<htmlTag>&& left,std::vector<htmlTag>&& right) noexcept;
+enum JsValueType{
+	JS_INT    = 0,
+	JS_DOUBLE = 1,
+	JS_NUM_MAX = 2,
+	JS_STRING = 3
+};
 
-std::vector<htmlTag> operator+(std::vector<htmlTag> left,const htmlTag& right) noexcept;
-std::vector<htmlTag> operator+(std::vector<htmlTag> left,htmlTag&& right) noexcept;
-std::vector<htmlTag>&& operator+(std::vector<htmlTag>&& left,const htmlTag& right) noexcept;
-std::vector<htmlTag>&& operator+(std::vector<htmlTag>&& left,htmlTag&& right) noexcept;
+enum NodeType{
+	NODE_VALUE,
+	NODE_EQ,
+	NODE_ADD,
+	NODE_SUB,
+	NODE_MUL,
+	NODE_DIV,
+};
 
-std::vector<htmlTag> operator+(std::vector<htmlTag> left,const htmlVal& right) noexcept;
-std::vector<htmlTag> operator+(std::vector<htmlTag> left,htmlVal&& right) noexcept;
-std::vector<htmlTag>&& operator+(std::vector<htmlTag>&& left,const htmlVal& right) noexcept;
-std::vector<htmlTag>&& operator+(std::vector<htmlTag>&& left,htmlVal&& right) noexcept;
+struct ExprNode
+{
+	const void* left;
+	const void* right;
+	NodeType type;
+};
+
+
+class JsValue{
+protected:
+	//state	
+	bool isStatic;
+	JsValueType valueType; 
+	
+	//for static value
+	void* staticValue;
+
+	//dor dynamic
+	ExprNode* node; 
+
+	void* copyStaticResource() const noexcept; 
+	void releaseResource() noexcept;
+	void deleteNode(const ExprNode* node) noexcept;
+	ExprNode* copyNode(const ExprNode& node) const;
+
+public:
+	JsValue(const JsValue& other);
+	JsValue(JsValue&& other);
+	JsValue(const JsValue& left,const JsValue& right,const NodeType& nodeType);
+	//JsValue(const JsValue& left,JsValue&&      right,const NodeType& nodeType);
+	//JsValue(JsValue&&      left,const JsValue& right,const NodeType& nodeType);
+	//JsValue(JsValue&&      left,JsValue&&      right,const NodeType& nodeType);
+	JsValue(const std::string& s);
+	JsValue(const int& i);
+	JsValue(const double& d);
+
+	~JsValue();
+
+	JsValue operator=(const JsValue& right) noexcept;
+	JsValue operator=(JsValue&& right) noexcept;
+	JsValue operator+(const JsValue& right) const;
+	JsValue operator-(const JsValue& right) const;
+	JsValue operator*(const JsValue& right) const;
+	JsValue operator/(const JsValue& right) const;
+};
 
--- lib/HtmlClass/src/html_class.cc
+++ lib/HtmlClass/src/html_class.cc
@@ -3,13 +3,13 @@
 
 
 //operator
-std::vector<htmlTag> operator+(const std::vector<htmlTag>& left  ,std::vector<htmlTag>&& right) noexcept{
+std::vector<HtmlTag> operator+(const std::vector<HtmlTag>& left  ,std::vector<HtmlTag>&& right) noexcept{
 	auto copyed_left = left;
 	if((left.empty() || right.empty()) || (left[0].getTagType() == right[0].getTagType()))
 		copyed_left.insert(copyed_left.end(), std::make_move_iterator(right.begin()), std::make_move_iterator(right.end()));
 	return std::move(copyed_left);
 }
-std::vector<htmlTag>&& operator+(std::vector<htmlTag>&& left,std::vector<htmlTag>&& right) noexcept{
+std::vector<HtmlTag>&& operator+(std::vector<HtmlTag>&& left,std::vector<HtmlTag>&& right) noexcept{
 	if((left.empty() || right.empty()) || (left[0].getTagType() == right[0].getTagType()))
 		left.insert(left.end(), std::make_move_iterator(right.begin()), std::make_move_iterator(right.end()));
 	return std::move(left);
@@ -17,46 +17,46 @@ std::vector<htmlTag>&& operator+(std::vector<htmlTag>&& left,std::vector<htmlTag
 
 
 
-std::vector<htmlTag> operator+(std::vector<htmlTag> left,const htmlTag& right) noexcept{
+std::vector<HtmlTag> operator+(std::vector<HtmlTag> left,const HtmlTag& right) noexcept{
 	if(left.empty() || left[0].getTagType() == TagType::TAG_TYPE_ELEM)
 		left.push_back(right);
 	return std::move(left);
 }
-std::vector<htmlTag> operator+(std::vector<htmlTag> left,htmlTag&& right) noexcept{
-	if(left.empty() || left[0].getTagType() == TagType::TAG_TYPE_ELEM)
-		left.push_back(std::move(right));
-	return std::move(left);
-}
-std::vector<htmlTag>&& operator+(std::vector<htmlTag>&& left,const htmlTag& right) noexcept{
-	if(left.empty() || left[0].getTagType() == TagType::TAG_TYPE_ELEM)
-		left.push_back(right);
-	return std::move(left);
-}
-std::vector<htmlTag>&& operator+(std::vector<htmlTag>&& left,htmlTag&& right) noexcept{
+std::vector<HtmlTag> operator+(std::vector<HtmlTag> left,HtmlTag&& right) noexcept{
 	if(left.empty() || left[0].getTagType() == TagType::TAG_TYPE_ELEM)
 		left.push_back(std::move(right));
 	return std::move(left);
 }
+std::vector<HtmlTag>&& operator+(std::vector<HtmlTag>&& left,const HtmlTag& right) noexcept{
+	if(left.empty() || left[0].getTagType() == TagType::TAG_TYPE_ELEM)
+		left.push_back(right);
+	return std::move(left);
+}
+std::vector<HtmlTag>&& operator+(std::vector<HtmlTag>&& left,HtmlTag&& right) noexcept{
+	if(left.empty() || left[0].getTagType() == TagType::TAG_TYPE_ELEM)
+		left.push_back(std::move(right));
+	return std::move(left);
+}
 
 
 
 
-std::vector<htmlTag> operator+(std::vector<htmlTag> left,const htmlVal& right) noexcept{
+std::vector<HtmlTag> operator+(std::vector<HtmlTag> left,const HtmlVal& right) noexcept{
 	if(left.empty() || left[0].getTagType() == TagType::TAG_TYPE_VALUE)
 		left.push_back(right);
 	return std::move(left);
 }
-std::vector<htmlTag> operator+(std::vector<htmlTag> left,htmlVal&& right) noexcept{
+std::vector<HtmlTag> operator+(std::vector<HtmlTag> left,HtmlVal&& right) noexcept{
 	if(left.empty() || left[0].getTagType() == TagType::TAG_TYPE_VALUE)
 		left.push_back(std::move(right));
 	return std::move(left);
 }
-std::vector<htmlTag>&& operator+(std::vector<htmlTag>&& left,const htmlVal& right) noexcept{
+std::vector<HtmlTag>&& operator+(std::vector<HtmlTag>&& left,const HtmlVal& right) noexcept{
 	if(left.empty() || left[0].getTagType() == TagType::TAG_TYPE_VALUE)
 		left.push_back(right);
 	return std::move(left);
 }
-std::vector<htmlTag>&& operator+(std::vector<htmlTag>&& left,htmlVal&& right) noexcept{
+std::vector<HtmlTag>&& operator+(std::vector<HtmlTag>&& left,HtmlVal&& right) noexcept{
 	if(left.empty() || left[0].getTagType() == TagType::TAG_TYPE_VALUE)
 		left.push_back(std::move(right));
 	return std::move(left);
@@ -73,61 +73,61 @@ std::vector<htmlTag>&& operator+(std::vector<htmlTag>&& left,htmlVal&& right) no
  */
 
 //コピー初期化
-htmlTag::htmlTag(const htmlTag& other) noexcept
+HtmlTag::HtmlTag(const HtmlTag& other) noexcept
 	:tag(other.tag),
 	option(other.option),
 	children(other.children),
 	type(other.type){};
 
 //ムーブ初期化
-htmlTag::htmlTag(htmlTag&& other) noexcept
+HtmlTag::HtmlTag(HtmlTag&& other) noexcept
 	:tag(std::move(other.tag)),
 	option(std::move(other.option)),
 	children(std::move(other.children)),
-	type(other.type){};
+	type(std::move(other.type)){};
 
 //子なし初期化
-htmlTag::htmlTag(const std::string& tag,const std::string& option) noexcept
+HtmlTag::HtmlTag(const std::string& tag,const std::string& option) noexcept
 	:option(option),children(),type(TagType::TAG_TYPE_ELEM){
-	this->tag = tag.substr(0,tag.find(' '));
-	//insert space for split tag and option
-	if(!this->option.empty() && this->option[0] != ' ')
-		this->option.insert(this->option.begin(),' ');
+		this->tag = tag.substr(0,tag.find(' '));
+		//insert space for split tag and option
+		if(!this->option.empty() && this->option[0] != ' ')
+			this->option.insert(this->option.begin(),' ');
 }
 
 //子なし初期化
-htmlTag::htmlTag(std::string&& tag,std::string&& option) noexcept
+HtmlTag::HtmlTag(std::string&& tag,std::string&& option) noexcept
 :option(option),children(),type(TagType::TAG_TYPE_ELEM){
-	this->tag = tag.substr(0,tag.find(' '));
-	//insert space for split tag and option
-	if(!this->option.empty() && this->option[0] != ' ')
-		this->option.insert(this->option.begin(),' ');
+		this->tag = tag.substr(0,tag.find(' '));
+		//insert space for split tag and option
+		if(!this->option.empty() && this->option[0] != ' ')
+			this->option.insert(this->option.begin(),' ');
 }
 
 //通常初期化
-htmlTag::htmlTag(const std::string& tag,const std::string& option,std::vector<htmlTag> children) noexcept
+HtmlTag::HtmlTag(const std::string& tag,const std::string& option,std::vector<HtmlTag> children) noexcept
 	:option(option),children(children),type(TagType::TAG_TYPE_ELEM){
-	this->tag = tag.substr(0,tag.find(' '));
-	//insert space for split tag and option
-	if(!this->option.empty() && this->option[0] != ' ')
-		this->option.insert(this->option.begin(),' ');
+		this->tag = tag.substr(0,tag.find(' '));
+		//insert space for split tag and option
+		if(!this->option.empty() && this->option[0] != ' ')
+			this->option.insert(this->option.begin(),' ');
 }
 
 //通常初期化
-htmlTag::htmlTag(std::string&& tag,std::string&& option,std::vector<htmlTag> children) noexcept
+HtmlTag::HtmlTag(std::string&& tag,std::string&& option,std::vector<HtmlTag> children) noexcept
 	:option(option),children(children),type(TagType::TAG_TYPE_ELEM){
-	this->tag = tag.substr(0,tag.find(' '));
-	//insert space for split tag and option
-	if(!this->option.empty() && this->option[0] != ' ')
-		this->option.insert(this->option.begin(),' ');
+		this->tag = tag.substr(0,tag.find(' '));
+		//insert space for split tag and option
+		if(!this->option.empty() && this->option[0] != ' ')
+			this->option.insert(this->option.begin(),' ');
 }
 
-htmlTag::~htmlTag() noexcept{
+HtmlTag::~HtmlTag() noexcept{
 
 }
 
 //encord
-std::string htmlTag::encord() const{
+std::string HtmlTag::encord() const{
 	std::string ret("");
 	if(this->getTagType() == TagType::TAG_TYPE_VALUE){
 		ret += this->option;
@@ -142,7 +142,7 @@ std::string htmlTag::encord() const{
 }
 
 //代入演算
-htmlTag& htmlTag::operator=(const htmlTag& other) noexcept{
+HtmlTag& HtmlTag::operator=(const HtmlTag& other) noexcept{
 	this->tag = other.tag;
 	this->type = other.type;
 	this->option = other.option;
@@ -151,7 +151,7 @@ htmlTag& htmlTag::operator=(const htmlTag& other) noexcept{
 }
 
 //ムーブ演算
-htmlTag& htmlTag::operator=(htmlTag&& other) noexcept{
+HtmlTag& HtmlTag::operator=(HtmlTag&& other) noexcept{
 	if(this != &other){
 		this->tag      = std::move(other.tag);
 		this->type     = std::move(other.type);
@@ -162,8 +162,8 @@ htmlTag& htmlTag::operator=(htmlTag&& other) noexcept{
 }
 
 //リストの連結
-std::vector<htmlTag> htmlTag::operator+(htmlTag& right) noexcept{
-	std::vector<htmlTag> res;
+std::vector<HtmlTag> HtmlTag::operator+(HtmlTag& right) noexcept{
+	std::vector<HtmlTag> res;
 	
 	if(this->getTagType() == TagType::TAG_TYPE_ELEM)
 		res.push_back(*this);
@@ -174,8 +174,8 @@ std::vector<htmlTag> htmlTag::operator+(htmlTag& right) noexcept{
 }
 
 //リストの連結
-std::vector<htmlTag> htmlTag::operator+(htmlTag&& right) noexcept{
-	std::vector<htmlTag> res;
+std::vector<HtmlTag> HtmlTag::operator+(HtmlTag&& right) noexcept{
+	std::vector<HtmlTag> res;
 	
 	if(this->getTagType() == TagType::TAG_TYPE_ELEM)
 		res.push_back(*this);
@@ -194,35 +194,35 @@ std::vector<htmlTag> htmlTag::operator+(htmlTag&& right) noexcept{
 
  
 //コピー初期化
-htmlVal::htmlVal(const htmlVal& other) noexcept:
-	htmlTag::htmlTag(other){};
+HtmlVal::HtmlVal(const HtmlVal& other) noexcept:
+	HtmlTag::HtmlTag(other){};
 
 //ムーブ初期化
-htmlVal::htmlVal(htmlVal&& other) noexcept:
-	htmlTag::htmlTag(std::move(other)){};
+HtmlVal::HtmlVal(HtmlVal&& other) noexcept:
+	HtmlTag::HtmlTag(std::move(other)){};
 
 //通常初期化
-htmlVal::htmlVal(const std::string& text) noexcept{
+HtmlVal::HtmlVal(const std::string& text) noexcept{
 	this->tag      = "";
 	this->type     = TagType::TAG_TYPE_VALUE;
 	this->option   = text;
-	this->children = std::vector<htmlTag>();
+	this->children = std::vector<HtmlTag>();
 }
 
 //通常初期化
-htmlVal::htmlVal(std::string&& text) noexcept{
+HtmlVal::HtmlVal(std::string&& text) noexcept{
 	this->tag      = "";
 	this->type     = TagType::TAG_TYPE_VALUE;
 	this->option   = std::move(text);
-	this->children = std::vector<htmlTag>();
+	this->children = std::vector<HtmlTag>();
 }
 
-htmlVal::~htmlVal() noexcept{
+HtmlVal::~HtmlVal() noexcept{
 
 }
 
 //ムーブ演算
-htmlVal& htmlVal::operator=(htmlVal&& other) noexcept{
+HtmlVal& HtmlVal::operator=(HtmlVal&& other) noexcept{
 	if(this != &other){
 		this->tag      = std::move(other.tag);
 		this->type     = std::move(other.type);
@@ -233,8 +233,8 @@ htmlVal& htmlVal::operator=(htmlVal&& other) noexcept{
 };
 
 //リストの連結
-std::vector<htmlTag> htmlVal::operator+(const htmlVal& right) const noexcept{
-	std::vector<htmlTag> res;
+std::vector<HtmlTag> HtmlVal::operator+(const HtmlVal& right) const noexcept{
+	std::vector<HtmlTag> res;
 	
 	if(this->getTagType() == TagType::TAG_TYPE_VALUE)
 		res.push_back(*this);
@@ -245,8 +245,8 @@ std::vector<htmlTag> htmlVal::operator+(const htmlVal& right) const noexcept{
 }
 
 //リストの連結
-std::vector<htmlTag> htmlVal::operator+(htmlVal&& right) const noexcept{
-	std::vector<htmlTag> res;
+std::vector<HtmlTag> HtmlVal::operator+(HtmlVal&& right) const noexcept{
+	std::vector<HtmlTag> res;
 	
 	if(this->getTagType() == TagType::TAG_TYPE_VALUE)
 		res.push_back(*this);
@@ -254,4 +254,349 @@ std::vector<htmlTag> htmlVal::operator+(htmlVal&& right) const noexcept{
 		res.push_back(std::move(right));
 		
 	return res;
+}
+
+
+
+
+
+
+/***********************************
+*           JsValue
+ * *****************************
+ */
+
+
+// 静的リソースのコピー
+void* JsValue::copyStaticResource() const noexcept{
+	switch (this->valueType)
+	{
+	case JsValueType::JS_STRING:
+		return new std::string(*(std::string*)this->staticValue);
+	case JsValueType::JS_INT:
+		return new int(*(int*)this->staticValue);
+	case JsValueType::JS_DOUBLE:
+		return new double(*(double*)this->staticValue);
+	default:
+		return nullptr;
+	}
+}
+
+//リソースの開放
+void JsValue::releaseResource() noexcept{
+	if(this->isStatic){
+		switch (this->valueType)
+		{
+		case JsValueType::JS_STRING:
+				delete (std::string*) this->staticValue;
+			break;
+		case JsValueType::JS_INT:
+				delete (int*) this->staticValue;
+			break;
+		case JsValueType::JS_DOUBLE:
+				delete (double*) this->staticValue;
+			break;
+		default:
+			break;
+		}
+	}else{
+		this->deleteNode(this->node);
+	}
+}
+
+void JsValue::deleteNode(const ExprNode* node) noexcept{
+	//deleate
+	if(node != nullptr){
+		if(node->type != NodeType::NODE_VALUE){
+			//if node
+			this->deleteNode((ExprNode*)node->left);
+			this->deleteNode((ExprNode*)node->right);
+		}else{
+			//if JsValue
+			delete (JsValue*)node->left;
+		}
+	}
+	delete node;
+}
+
+// ノードコピー
+ExprNode* JsValue::copyNode(const ExprNode& node) const{
+	if(node.type == NodeType::NODE_VALUE){
+		return new ExprNode(node);
+	}else{
+		ExprNode newNode;
+		newNode.type = node.type;
+		newNode.left = JsValue::copyNode(*(ExprNode*)node.left);
+		newNode.right = JsValue::copyNode(*(ExprNode*)node.right);
+		return new ExprNode(newNode);
+	}
+}
+
+//コピー初期化
+JsValue::JsValue(const JsValue& other):
+	isStatic(other.isStatic),
+	staticValue(other.staticValue),
+	valueType(other.valueType),
+	node(other.node){}
+
+//ムーブ初期化
+JsValue::JsValue(JsValue&& other):
+	isStatic(std::move(other.isStatic)),
+	staticValue(std::move(other.staticValue)),
+	valueType(std::move(other.valueType)),
+	node(std::move(other.node)){}
+
+// node 
+JsValue::JsValue(const JsValue& left,const JsValue& right,const NodeType& nodeType):
+	isStatic(false),
+	staticValue(nullptr),
+	valueType(left.valueType){
+		
+	ExprNode* newNode = new ExprNode;
+	//set type
+	newNode->type = nodeType;
+
+	//set left
+	if(left.isStatic){
+		newNode->left = new ExprNode;
+		((ExprNode*)newNode->left)->type = NodeType::NODE_VALUE;
+		((ExprNode*)newNode->left)->left = new JsValue(left);
+	}else{
+		newNode->left = JsValue::copyNode(*left.node);
+	}
+	
+	//set right
+	if(right.isStatic){
+		newNode->right = new ExprNode;
+		((ExprNode*)newNode->right)->type = NodeType::NODE_VALUE;
+		((ExprNode*)newNode->right)->left = new JsValue(right);
+	}else{
+		newNode->right = JsValue::copyNode(*right.node);
+	}
+
+	this->node = newNode;
+}
+//JsValue::JsValue(const JsValue& left,JsValue&&      right);
+//JsValue::JsValue(JsValue&&      left,const JsValue& right);
+//JsValue::JsValue(JsValue&&      left,JsValue&&      right):
+
+// string
+JsValue::JsValue(const std::string& s):
+	isStatic(true),
+	node(nullptr),
+	valueType(JsValueType::JS_STRING){
+		this->staticValue = new std::string(s);
+}
+
+// int
+JsValue::JsValue(const int& i):
+	isStatic(true),
+	node(nullptr),
+	valueType(JsValueType::JS_INT){
+		this->staticValue = new int(i);
+}
+
+// double
+JsValue::JsValue(const double& d):
+	isStatic(true),
+	node(nullptr),
+	valueType(JsValueType::JS_DOUBLE){
+		this->staticValue = new double(d);
+}
+
+// デストラクタ
+JsValue::~JsValue(){
+	this->releaseResource();
+}
+
+// 代入
+JsValue JsValue::operator=(const JsValue& right) noexcept{
+	this->releaseResource();
+	this->valueType = right.valueType;
+
+	if(right.isStatic){
+		this->staticValue = right.copyStaticResource();
+	}else{
+		this->node = new ExprNode(*right.node);
+	}
+}
+
+// ムーブ代入
+JsValue JsValue::operator=(JsValue&& right) noexcept{
+	this->releaseResource();
+	this->valueType = std::move(right.valueType);
+
+	if(right.isStatic){
+		this->staticValue = std::move(right.staticValue);
+		right.staticValue = nullptr;
+	}else{
+		this->node = std::move(right.node);
+		right.node = nullptr;
+	}
+}
+
+///////// 演算子
+
+JsValue JsValue::operator+(const JsValue& right) const{
+	// check assert
+	if(this->valueType != right.valueType && std::max(this->valueType,right.valueType) > JsValueType::JS_NUM_MAX){
+		throw "JsValue::operator+: value type is invalid.";
+	}
+	
+	//check static
+	if(this->isStatic ^ right.isStatic){
+		// switch by value type
+		switch (this->valueType)
+		{
+		//left is int
+		case JsValueType::JS_INT:
+			switch (right.valueType)
+			{
+			//right is int
+			case JsValueType::JS_INT:
+				return JsValue((*(int*)this->staticValue) + (*(int*)right.staticValue));
+			//right is double
+			case JsValueType::JS_DOUBLE:
+				return JsValue((*(int*)this->staticValue) + (*(double*)right.staticValue));
+			}
+		
+		//left is double
+		case JsValueType::JS_DOUBLE:
+			switch (right.valueType)
+			{
+			//right is int
+			case JsValueType::JS_INT:
+				return JsValue((*(double*)this->staticValue) + (*(int*)right.staticValue));
+			//right is double
+			case JsValueType::JS_DOUBLE:
+				return JsValue((*(double*)this->staticValue) + (*(double*)right.staticValue));
+			}
+
+		// left is string
+		case JsValueType::JS_STRING:
+			return JsValue((*(std::string*)this->staticValue) + (*(std::string*)right.staticValue));
+		}
+	}else{
+		return JsValue(*this,right,NodeType::NODE_ADD);
+	}
+}
+
+JsValue JsValue::operator-(const JsValue& right) const{
+	// check assert
+	if(std::max(this->valueType,right.valueType) > JsValueType::JS_NUM_MAX){
+		throw "JsValue::operator+: value type is invalid.";
+	}
+	
+	//check static
+	if(this->isStatic ^ right.isStatic){
+		// switch by value type
+		switch (this->valueType)
+		{
+		//left is int
+		case JsValueType::JS_INT:
+			switch (right.valueType)
+			{
+			//right is int
+			case JsValueType::JS_INT:
+				return JsValue((*(int*)this->staticValue) - (*(int*)right.staticValue));
+			//right is double
+			case JsValueType::JS_DOUBLE:
+				return JsValue((*(int*)this->staticValue) - (*(double*)right.staticValue));
+			}
+		
+		//left is double
+		case JsValueType::JS_DOUBLE:
+			switch (right.valueType)
+			{
+			//right is int
+			case JsValueType::JS_INT:
+				return JsValue((*(double*)this->staticValue) - (*(int*)right.staticValue));
+			//right is double
+			case JsValueType::JS_DOUBLE:
+				return JsValue((*(double*)this->staticValue) - (*(double*)right.staticValue));
+			}
+		}
+	}else{
+		return JsValue(*this,right,NodeType::NODE_SUB);
+	}
+}
+
+JsValue JsValue::operator*(const JsValue& right) const{
+	// check assert
+	if(std::max(this->valueType,right.valueType) > JsValueType::JS_NUM_MAX){
+		throw "JsValue::operator+: value type is invalid.";
+	}
+	
+	//check static
+	if(this->isStatic ^ right.isStatic){
+		// switch by value type
+		switch (this->valueType)
+		{
+		//left is int
+		case JsValueType::JS_INT:
+			switch (right.valueType)
+			{
+			//right is int
+			case JsValueType::JS_INT:
+				return JsValue((*(int*)this->staticValue) - (*(int*)right.staticValue));
+			//right is double
+			case JsValueType::JS_DOUBLE:
+				return JsValue((*(int*)this->staticValue) - (*(double*)right.staticValue));
+			}
+		
+		//left is double
+		case JsValueType::JS_DOUBLE:
+			switch (right.valueType)
+			{
+			//right is int
+			case JsValueType::JS_INT:
+				return JsValue((*(double*)this->staticValue) - (*(int*)right.staticValue));
+			//right is double
+			case JsValueType::JS_DOUBLE:
+				return JsValue((*(double*)this->staticValue) - (*(double*)right.staticValue));
+			}
+		}
+	}else{
+		return JsValue(*this,right,NodeType::NODE_MUL);
+	}
+}
+
+JsValue JsValue::operator/(const JsValue& right) const{
+	// check assert
+	if(std::max(this->valueType,right.valueType) > JsValueType::JS_NUM_MAX){
+		throw "JsValue::operator+: value type is invalid.";
+	}
+	
+	//check static
+	if(this->isStatic ^ right.isStatic){
+		// switch by value type
+		switch (this->valueType)
+		{
+		//left is int
+		case JsValueType::JS_INT:
+			switch (right.valueType)
+			{
+			//right is int
+			case JsValueType::JS_INT:
+				return JsValue((*(int*)this->staticValue) / (*(int*)right.staticValue));
+			//right is double
+			case JsValueType::JS_DOUBLE:
+				return JsValue((*(int*)this->staticValue) / (*(double*)right.staticValue));
+			}
+		
+		//left is double
+		case JsValueType::JS_DOUBLE:
+			switch (right.valueType)
+			{
+			//right is int
+			case JsValueType::JS_INT:
+				return JsValue((*(double*)this->staticValue) / (*(int*)right.staticValue));
+			//right is double
+			case JsValueType::JS_DOUBLE:
+				return JsValue((*(double*)this->staticValue) / (*(double*)right.staticValue));
+			}
+		}
+	}else{
+		return JsValue(*this,right,NodeType::NODE_DIV);
+	}
 }
