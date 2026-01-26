9beed695f9c2bff11ed621b1c5fe8c9a163f5fce
user-0021 <nabeyamaoden@gmail.com>
2026-01-26 05:23:48 +0000

    a

--- main.cc
+++ main.cc
@@ -2,70 +2,32 @@
 #include <html_class.hpp>
 #include <iostream>
 
-#define HTTPS_CALLBACK() [=](const drogon::HttpRequestPtr &req,std::function<void(const drogon::HttpResponsePtr &)> &&callback)
+#define HTTPS_CALLBACK [=](const drogon::HttpRequestPtr &req,std::function<void(const drogon::HttpResponsePtr &)> &&callback)
 
 int main() {
-
-
-	HtmlPage mainPage;
-	mainPage.setHead(
-		HtmlTag("head",""
-		)
-	);
-	mainPage.setBody(
-		HtmlTag("body","option1=1",
-			HtmlTag("h1","option1=2",
-				HtmlVal("hello")
-			)+
-			HtmlTag("h2","option1=2",
-				HtmlVal("world" + std::to_string(100))
-			)+
-			HTML_TAG_FOREACH_BEGIN(int i = 0;i < 10;i++){
-				return 	HtmlTag("h2","option1=2",
-									HtmlVal("world" + std::to_string(i))
-								);
-			}
-			HTML_TAG_FOREACH_END
-		)
-	);
-	
-
 	//bind web page
-	drogon::app()
-		.registerHandler("/",
-			HTTPS_CALLBACK(){
-				auto resp = drogon::HttpResponse::newHttpResponse();
-				resp->setStatusCode(drogon::k200OK);
-				resp->setContentTypeCode(drogon::CT_TEXT_HTML);
-				resp->setBody(
-					//mainPage.encord()
-					HtmlTag("","",
-						HtmlTag("head",""
-						)+
-						HtmlTag("body","option1=1",
-							HtmlTag("h1","option1=2",
-								HtmlVal("hello")
-							)+
-							HtmlTag("h2","option1=2",
-								HtmlVal("world" + std::to_string(100))
-							)+
-							HTML_TAG_FOREACH_BEGIN(int i = 0;i < 10;i++){
-								return 	HtmlTag("h2","option1=2",
-													HtmlVal("world" + std::to_string(i))
-												);
-							}
-							HTML_TAG_FOREACH_END
-						)
-					).encord()
-				);
-
-				callback(resp);
-			});
 
 	//Set HTTP listener address and port
 	drogon::app().addListener("0.0.0.0", 5555);
 	//Load config file
-	//drogon::app().loadConfigFile("../config.json");
+	drogon::app().loadConfigFile("../config.json");
+
+	//regist callback
+	drogon::app().registerHandler(".*",HTTPS_CALLBACK{
+
+		//api callback
+		if (req->path().find("/api/") == 0) {
+			auto resp = drogon::HttpResponse::newNotFoundResponse();
+			callback(resp);
+			return;
+		}
+
+		// webpage
+		printf("%s\r\n",drogon::app().getHomePage().c_str());
+		auto resp = drogon::HttpResponse::newFileResponse(drogon::app().getHomePage());
+		callback(resp);
+	});
+
 	//drogon::app().loadConfigFile("../config.yaml");
 	//Run HTTP framework,the method will block in the internal event loop
 	drogon::app().run();
